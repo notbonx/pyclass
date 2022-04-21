2f6796fe006584c1d7957c4de798cff609703b75
антоша)) <notbonx@gmail.com>
2022-04-21 11:27:00 +0300

    super() not working with return like getter

--- tets.py
+++ tets.py
@@ -1,6 +1,23 @@
-with open('test.txt', 'r') as file:
-    while True:
-        now = file.readline()
-        if now == '':
-            break
-        print(now, end='')
+class Dad:
+    def __init__(self, arr):
+        self.arr = arr
+
+    def date(self):
+        print(self.arr, end=" ")
+
+class Son(Dad):
+    def __init__(self, arr, brr):
+        Dad.__init__(self, arr)
+        self.brr = brr
+
+    def date(self):
+        super().date()
+        print(self.brr)
+
+def main():
+    we = Son(1000, 1)
+    we.date()
+
+
+if __name__ == "__main__":
+    main()
